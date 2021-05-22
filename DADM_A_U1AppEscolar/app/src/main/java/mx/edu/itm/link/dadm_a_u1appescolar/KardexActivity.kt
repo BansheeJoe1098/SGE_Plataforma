47e4cf1bb7dcce6ac7fc96ef0f89d9e1ad6ac135
Joe Miguel <miguerdz1023@gmail.com>
2021-05-21 19:51:22 -0500

    error

--- DADM_A_U1AppEscolar/app/src/main/java/mx/edu/itm/link/dadm_a_u1appescolar/KardexActivity.kt
+++ DADM_A_U1AppEscolar/app/src/main/java/mx/edu/itm/link/dadm_a_u1appescolar/KardexActivity.kt
@@ -2,9 +2,11 @@ package mx.edu.itm.link.dadm_a_u1appescolar
 
 import android.app.Activity
 import android.content.Intent
+import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Toast
+import androidx.annotation.RequiresApi
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import mx.edu.itm.link.dadm_a_u1appescolar.adapters.RecyclerKardexAdapter
@@ -16,6 +18,7 @@ class KardexActivity : AppCompatActivity() {
 
     lateinit var recyclerKardex : RecyclerView
 
+    @RequiresApi(Build.VERSION_CODES.KITKAT)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_kardex)
